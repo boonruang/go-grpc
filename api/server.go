da78943470c0eac2a9d6f6f7da86edfbe8a660ca
boonruang <scubatoy@gmail.com>
2023-04-18 00:26:18 +0700

    lession#37 manage user session with refresh token

--- api/server.go
+++ api/server.go
@@ -44,9 +44,9 @@ func (server *Server) setupRouter() {
 	router := gin.Default()
 	router.POST("/users", server.createUser)
 	router.POST("/users/login", server.loginUser)
+	router.POST("/tokens/renew_access", server.renewAccessToken)
 
 	authRoutes := router.Group("/").Use(authMiddleware(server.tokenMaker))
-
 	authRoutes.POST("/accounts", server.createAccount)
 	authRoutes.GET("/accounts/:id", server.getAccount)
 	authRoutes.GET("/accounts", server.ListAccount)
